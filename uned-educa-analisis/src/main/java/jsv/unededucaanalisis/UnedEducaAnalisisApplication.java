45f104d70f4b61911df2478c9cb02a603b0918c2
Juanmanuelsanvi <juanmanuelsanvi@gmail.com>
2023-04-10 19:35:00 +0200

    renaming packages

--- uned-educa-analisis/src/main/java/jsv/unededucaanalisis/UnedEducaAnalisisApplication.java
+++ uned-educa-analisis/src/main/java/jsv/unededucaanalisis/UnedEducaAnalisisApplication.java
@@ -1,6 +1,5 @@
 package jsv.unededucaanalisis;
 
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- uned-educa-analisis/src/test/java/jsv/unededucaanalisis/UnedEducaAnalisisApplicationTests.java
+++ uned-educa-analisis/src/test/java/jsv/unededucaanalisis/UnedEducaAnalisisApplicationTests.java
@@ -1,4 +1,4 @@
-package amm.unededucaanalisis;
+package jsv.unededucaanalisis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
